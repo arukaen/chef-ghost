53f5eb8003f7b219db81c6f2c4edf613aaed7b63
arukaen <c@cristhekid.com>
2015-04-04 23:21:54 -0400

    more progress

--- recipes/_services.rb
+++ recipes/_services.rb
@@ -2,3 +2,8 @@
     supports :status => true, :restart => true, :reload => true
     action   :start
  end
+
+ service 'ghost' do
+    supports :status => true, :restart => true, :reload => true
+    action   :nothing
+ end
