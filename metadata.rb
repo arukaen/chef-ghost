b462b91723dd3e5b11c2014ed16188d9542e6f8c
arukaen <c@cristhekid.com>
2015-04-08 11:14:07 -0400

    stove

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'c@cristhekid.com'
 license          'Apache 2.0'
 description      'Installs & configures Ghost: open source blogging platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 %w{ ubuntu }.each do |os|
     supports os
