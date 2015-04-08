682d0f85040cc60286b43a248ea2a2d6882c58c0
arukaen <c@cristhekid.com>
2015-04-08 10:50:34 -0400

    changes to upload to supermarket

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,11 @@
 name             'ghostblog'
 maintainer       'Cris Gallardo'
 maintainer_email 'c@cristhekid.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs & configures Ghost: open source blogging platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
-supports 'ubuntu', '>= 12.04'
-supports 'ubuntu', '>= 14.04'
+supports 'ubuntu'
 
 depends 'nodejs', '~> 2.4.0'
