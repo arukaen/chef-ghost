4225f4c49970e758acd4a3fca0b10f0cc45de9a5
arukaen <c@cristhekid.com>
2015-04-08 10:57:34 -0400

    changes to metadata for supermarket #2

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      'Installs & configures Ghost: open source blogging platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
-supports 'ubuntu'
+%w{ ubuntu }.each do |os|
+    supports os
+end
 
 depends 'nodejs', '~> 2.4.0'
