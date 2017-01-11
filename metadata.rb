af642320f31dac17f22442be9297f31b37ffe5f6
arukaen <cris@invisionapp.com>
2017-01-11 14:01:00 -0500

    node & kitchen updates

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'c@cristhekid.com'
 license          'Apache 2.0'
 description      'Installs & configures Ghost: open source blogging platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.6'
+version          '1.0.7'
 
 %w( ubuntu ).each do |os|
     supports os
