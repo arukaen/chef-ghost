ba584bdbcf01b2cb360c0705779671194cbec4f4
Yorgos Saslis <g.saslis@gmail.com>
2017-01-12 08:59:24 +0200

    Removed ghost service from services recipe, as there is now a separate service per ghost blog

--- recipes/_services.rb
+++ recipes/_services.rb
@@ -2,8 +2,3 @@
     supports :status => true, :restart => true, :reload => true
     action   :nothing
  end
-
- service 'ghost' do
-    supports :status => true, :restart => true, :reload => true, :start => true, :stop => true
-    action   :nothing
- end
