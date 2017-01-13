75ac31602e118aa578c06a1e60587e42f1e4742c
Cris <arukaen@users.noreply.github.com>
2017-01-13 14:58:29 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'c@cristhekid.com'
 license          'Apache 2.0'
 description      'Installs & configures Ghost: open source blogging platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.8'
+version          '1.1.0'
 
 source_url 'https://github.com/arukaen/chef-ghost'
 issues_url 'https://github.com/arukaen/chef-ghost/issues'
