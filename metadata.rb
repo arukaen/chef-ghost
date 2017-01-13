71ff621ebc70dd6705f8d7091a6688cb5801348e
Yorgos Saslis <g.saslis@gmail.com>
2017-01-13 21:35:44 +0200

    Major version bump to indicate significant changes / rewrite / breaking backwards compatibility

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,7 @@ maintainer_email 'c@cristhekid.com, yorgo@protonmail.com'
 license          'Apache 2.0'
 description      'Installs & configures Ghost: open source blogging platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
-
-source_url 'https://github.com/arukaen/chef-ghost'
-issues_url 'https://github.com/arukaen/chef-ghost/issues'
+version          '2.0.0'
 
 %w( ubuntu ).each do |os|
     supports os
