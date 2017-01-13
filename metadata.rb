7c02aa8e2ca7fcd1840a7ce8271a3583cd69f81e
Yorgos Saslis <g.saslis@gmail.com>
2017-01-13 21:32:14 +0200

    Added my name to the maintainers

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'ghost-blog'
-maintainer       'Cris Gallardo'
-maintainer_email 'c@cristhekid.com'
+maintainer       'Cris Gallardo, Yorgos Saslis'
+maintainer_email 'c@cristhekid.com, yorgo@protonmail.com'
 license          'Apache 2.0'
 description      'Installs & configures Ghost: open source blogging platform'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
