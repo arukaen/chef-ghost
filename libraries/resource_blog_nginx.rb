acf822aa63246a5bba244a1bc28140740f70c4cf
Stephen Sadowski <stephen.sadowski@icfolson.com>
2017-03-17 11:24:21 -0500

    Cleaned up cruft for kitchen tests Updated metadata to reflect specific platform support Updated resource_blog_nginx to correctly accept an ssl parameter Full centos-7 support with kitchen convergence

--- libraries/resource_blog_nginx.rb
+++ libraries/resource_blog_nginx.rb
@@ -11,9 +11,11 @@ class Chef
       property :blog_name, String, required: true
       property :blog_domain, String, required: true
       property :proxy_port, Integer, required: true
+      property :ssl
 
       action :create do
         nginx_attrs = node['ghost-blog']['nginx'].to_h
+
         nginx_attrs['ssl_certificate'] ||= "#{nginx_attrs['dir']}/ssl/#{blog_name}.crt"
         nginx_attrs['ssl_certificate_key'] ||= "#{nginx_attrs['dir']}/ssl/#{blog_name}.key"
         nginx_attrs['self_signed_ssl_certificate_subj'] ||=
@@ -22,6 +24,13 @@ class Chef
         nginx_attrs['blog_name'] ||= blog_name
         nginx_attrs['blog_domain'] ||= blog_domain
         nginx_attrs['port'] ||= proxy_port
+        # a little different because the above aren't defined by default
+        # override if we're setting in the resource block
+        nginx_attrs['ssl'] = ssl if defined? ssl
+
+        print "---- NGINX ATTRS ----"
+        print nginx_attrs.to_s
+        print "---------------------"
 
         # Bring in the latest stable nginx from apt (will not upgrade, though)
         apt_repository 'nginx' do
@@ -31,33 +40,39 @@ class Chef
 
         package 'nginx'
 
-        # Utilities to enable and disable nginx sites
-        %w(nxensite nxdissite).each do |nxscript|
-          template "#{nginx_attrs['script_dir']}/#{nxscript}" do
-            source "#{nxscript}.erb"
-            cookbook 'ghost-blog'
-            mode '0755'
-            owner 'root'
-            group 'root'
+        case node[:platform_family]
+        when 'debian'
+          # Utilities to enable and disable nginx sites
+          %w(nxensite nxdissite).each do |nxscript|
+            template "#{nginx_attrs['script_dir']}/#{nxscript}" do
+              source "#{nxscript}.erb"
+              cookbook 'ghost-blog'
+              mode '0755'
+              owner 'root'
+              group 'root'
+            end
           end
         end
 
-        # Self-signed certificate (if needed)
-        execute "generate self-signed cert #{nginx_attrs['self_signed']}" do
-          command "openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-            -keyout #{nginx_attrs['ssl_certificate_key'].to_s.shellescape} \
-            -out #{nginx_attrs['ssl_certificate'].to_s.shellescape}
-            --subj #{nginx_attrs['self_signed_ssl_certificate_subj'].to_s.shellescape}"
-          # TODO: regen or extend if expired
-          only_if do
-            # We don't overwrite keys and certificates. Not our jam, yo.
-            if nginx_attrs['ssl'] &&
-               !::File.exist?(nginx_attrs['ssl_certificate']) &&
-               !::File.exist?(nginx_attrs['ssl_certificate_key'])
+        if nginx_attrs['ssl'] then
+          # Self-signed certificate (if needed)
+          execute "generate self-signed cert #{nginx_attrs['self_signed']}" do
+            command "openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+              -keyout #{nginx_attrs['ssl_certificate_key'].to_s.shellescape} \
+              -out #{nginx_attrs['ssl_certificate'].to_s.shellescape}
+              --subj #{nginx_attrs['self_signed_ssl_certificate_subj'].to_s.shellescape}"
+            # TODO: regen or extend if expired
+            only_if do
+              # We don't overwrite keys and certificates. Not our jam, yo.
+              if nginx_attrs['ssl'] &&
+                 !::File.exist?(nginx_attrs['ssl_certificate']) &&
+                 !::File.exist?(nginx_attrs['ssl_certificate_key'])
+              end
             end
+            notifies :restart, 'service[nginx]'
           end
-          notifies :restart, 'service[nginx]'
         end
+
 
         # Create the server definition
         case node[:platform_family]
